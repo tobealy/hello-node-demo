acdbe6231c481a9971a64d074d4dabff965aa056
alyibrahim <aly.ibrahim@gmail.com>
2020-10-28 17:21:58 +0000

    Title change

--- hello-node/routes/index.js
+++ hello-node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Aly' });
+  res.render('index', { title: 'Red Hat' });
 });
 
 module.exports = router;
