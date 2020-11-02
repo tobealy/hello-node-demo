54463f66d02889ecd4d09c3dda690f23914bacf7
alyibrahim <aly.ibrahim@gmail.com>
2020-11-02 16:29:52 +0000

    Update green

--- hello-node/routes/index.js
+++ hello-node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Sysco' });
+  res.render('index', { title: 'Aly' });
 });
 
 module.exports = router;
